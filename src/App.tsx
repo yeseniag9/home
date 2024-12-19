6bf0d8a0392a8a576e8c4f8e8acf74ca2d3735dd
Yesenia <yeseniaaag9@gmail.com>
2024-12-18 23:09:27 -0600

    deleted button for basic vite project

--- src/App.tsx
+++ src/App.tsx
@@ -17,9 +17,6 @@ function App() {
         </a>
       </div>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
