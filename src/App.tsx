2d442b5563acdcf3be5134569f5da5b46361b053
Yesenia <yeseniaaag9@gmail.com>
2024-12-19 23:05:50 -0600

    removed div with react logo

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
     </>
   )
 }
